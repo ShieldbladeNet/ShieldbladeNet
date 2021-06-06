69dd8d5d98c75f476c60c58f0096503dee55949e
ShieldbladeNet <85451600+ShieldbladeNet@users.noreply.github.com>
2021-06-07 00:07:46 +0700

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 import requests, json, random, time
 timestr = time.strftime("%Y-%m-%d - %H:%M:%S UTC")
+nameid = "ShieldbladeNet"
 f = open("./README.md", "w")
 
 f.write(f'''
@@ -7,15 +8,21 @@ f.write(f'''
 <h3 align="center"><b>{timestr}</b></h3>
 <h3 align="center">Have a nice day!</h3>
 <p align="center">
-  <a href="https://github.com/ShieldbladeNet">
-    <img alt="GitHub Stats" src="https://github-readme-stats.vercel.app/api?username=ShieldbladeNet&hide=issues&hide_title=true&include_all_commits=true&bg_color=30,e96443,904e95&title_color=fff&text_color=fff" />
+  <a href="https://github.com/{nameid}">
+    <img alt="GitHub Stats" src="https://github-readme-stats.vercel.app/api?username={nameid}&hide=issues&hide_title=true&include_all_commits=true&bg_color=30,e96443,904e95&title_color=fff&text_color=fff" />
     </a>
 </p>
 <p align="center">
 
-[![GitHub release (latest by date)](https://img.shields.io/github/v/release/ShieldbladeNet/Nano-Openwrt?style=for-the-badge&label=Download)](https://github.com/ShieldbladeNet/Nano-Openwrt/releases)
+[![GitHub release (latest by date)](https://img.shields.io/github/v/release/{nameid}/Nano-Openwrt?style=for-the-badge&label=Download)](https://github.com/{nameid}/Nano-Openwrt/releases)
 
 </p>       
+
+<p align="center">
+
+[![build_firmware](https://github.com/{nameid}/Nano-Openwrt/actions/workflows/build-firmware.yml/badge.svg)](https://github.com/{nameid}/Nano-Openwrt/actions/workflows/build-firmware.yml)
+
+</p> 
 
 OpenWrt (from open wireless router) is an open-source project for embedded operating systems based on Linux, primarily used on embedded devices to route network traffic. The main components are Linux, util-linux, musl,[4] and BusyBox. All components have been optimized to be small enough to fit into the limited storage and memory available in home routers.
 
@@ -23,7 +30,7 @@ OpenWrt is configured using a command-line interface (ash shell) or a web interf
 
 OpenWrt can run on various types of devices, including CPE routers, residential gateways, smartphones, pocket computers (e.g. Ben NanoNote). It is also possible to run OpenWrt on personal computers and laptops, which are most commonly based on the x86 architecture.
 
-[![build_firmware](https://github.com/ShieldbladeNet/Nano-Openwrt/actions/workflows/build-firmware.yml/badge.svg)](https://github.com/ShieldbladeNet/Nano-Openwrt/actions/workflows/build-firmware.yml)
+
 
 ''')
 f.close()
